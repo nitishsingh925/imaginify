1a545c3421080888588afebe97e8e3957db3a996
Nitish Singh <nitishsingh925@gmail.com>
2024-03-17 19:11:45 +0530

    Layout Sidebar & Mobile Navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
 import { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
